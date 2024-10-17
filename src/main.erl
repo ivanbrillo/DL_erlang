2e3d0902d6c4b600321639bfab16230be41c8d2d
ivanbrillo <brilloivan2@gmail.com>
2024-10-17 13:31:40 +0200

    updated console output

--- src/main.erl
+++ src/main.erl
@@ -10,14 +10,18 @@
 start_master_slave() ->
     PythonCodePath = code:priv_dir(ds_proj),
     {ok, Master} = python:start([{python_path, PythonCodePath}, {python, "python3"}]),
+    io:format("Master start correctly~n"),
+
     {ok, Slave} = python:start([{python_path, PythonCodePath}, {python, "python3"}]),
+    io:format("Slave 1 start correctly~n"),
+
     {Master, Slave}.
 
 
 get_model(Master) ->
     RawModel = python:call(Master, master, get_model_definition, []),
-    io:format("Raw data from Python (first 100 chars):~n~p~n", [string:slice(RawModel, 0, 100)]),
     Model = list_to_binary(RawModel),
+    io:format("Model retrieved correctly from Master~n"),
     Model.
     % ModelData = jsx:decode(Model, [return_maps]),
     % ProcessedModel = jsx:encode(ModelData),
@@ -25,7 +29,7 @@ get_model(Master) ->
 
 get_weights(Master) ->
     RawWeights = python:call(Master, master, get_model_weights, []),
-    io:format("Raw data from Python (first 100 chars):~n~p~n", [string:slice(RawWeights, 0, 100)]),
+    % io:format("Raw data from Python (first 100 chars):~n~p~n", [string:slice(RawWeights, 0, 100)]),
     Weights = list_to_binary(RawWeights),
     Weights. 
 
@@ -33,9 +37,13 @@ get_weights(Master) ->
 initialize_model(Master, Slave, NodeId) ->
     Model = get_model(Master),
     python:call(Slave, node, register_handler, [self(), NodeId]),
-    Slave ! {initialize, Model}.
+    Slave ! {initialize, Model},
+    io:format("Slave ~p model initialized correctly~n", [NodeId]),
+    ok.
 
 
 update_weights(Master, Slave) ->
     Weights = get_weights(Master),
-    Slave ! {update, Weights}.
+    Slave ! {update, Weights},
+    io:format("Slave 1 weights updated correctly~n"),
+    ok.
