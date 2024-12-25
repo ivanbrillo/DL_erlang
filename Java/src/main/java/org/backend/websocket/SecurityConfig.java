a2c0e454a32d85c769d97c9437e5c1df3d0d546a
ivanbrillo <brilloivan2@gmail.com>
2024-12-25 10:31:48 +0100

    with sessioID

--- Java/src/main/java/org/backend/websocket/SecurityConfig.java
+++ Java/src/main/java/org/backend/websocket/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,13 +20,15 @@ public class SecurityConfig {
                         .requestMatchers("/login").permitAll() // Allow access to login and static resources
                         .anyRequest().authenticated() // Require authentication for every other request
                 )
-                .formLogin(withDefaults())// Ensure the login page is the first thing they see
-                .httpBasic(withDefaults()); // Keep basic authentication if needed (but formLogin will take precedence)
+                .formLogin(withDefaults()) // Use the default Spring Security login page
+                .httpBasic(withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .invalidSessionUrl("/login?expired")
+                        .maximumSessions(1)
+                        .expiredUrl("/login?expired")
+                );
 
         return http.build();
     }
-
-
-
-
 }
